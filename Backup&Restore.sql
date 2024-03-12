c81efb80bb8237f0c8bdf949af6eea42e683f162
Denner-Maia <dennermaia22@gmail.com>
2024-03-12 11:00:00 -0300

    Update Backup&Restore.sql

--- Backup&Restore.sql
+++ Backup&Restore.sql
@@ -1,5 +1,18 @@
 
 
+	--Query para conferir o histórico de Backups que foram executados
+
+    SELECT	database_name, name,backup_start_date, datediff(mi, backup_start_date, backup_finish_date) [tempo (min)],
+		position, server_name, recovery_model, isnull(logical_device_name, ' ') logical_device_name, device_type, 
+		type, cast(backup_size/1024/1024 as numeric(15,2)) [Tamanho (MB)]
+FROM msdb.dbo.backupset B
+	  INNER JOIN msdb.dbo.backupmediafamily BF ON B.media_set_id = BF.media_set_id
+where backup_start_date >=  dateadd(hh, -24 ,getdate()  )
+--  and type in ('D','I')
+order by backup_start_date desc
+
+--	Guardem muito bem essa query que utilizaram uma infinidade de vezes como DBA para conferir Backups!!!
+--	D = FULL, I = Diferencial, L = Log
 
 
 
@@ -25,6 +38,11 @@ WITH INIT, COMPRESSION, CHECKSUM,NAME = 'Destino\Nome_arquivo.bak'
 
 
 ---- Restore criando uma nova database
+
+  --	Apenas para validar o nome lógico dos arquivos de dados e logs
+RESTORE FILELISTONLY
+FROM DISK = 'Caminho onde se encontra o arquivo.bak'
+  
 RESTORE DATABASE Nome_Banco
 FROM DISK = 'Caminho\Nome_arquivo.bak'
 WITH RECOVERY,STATS = 5,
@@ -55,6 +73,11 @@ WITH NORECOVERY
 restore database Nome_banco with recovery
 
 
+--Restore do Log com STOP AT
+--	Restore Log até determinado horário(hora, minuto, segundo, centésimos)
+RESTORE LOG TreinamentoDBA
+FROM DISK = 'Caminho do Arquivo.bak'
+WITH RECOVERY,STOPAT = 'ano/mes/dia hora:minuto:segundo.centésimos'
 
 
 
